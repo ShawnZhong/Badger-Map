9449667b4ce0c0ae26f332e4f8b5eae0c7022d20
Shawn Zhong <public@shawnzhong.com>
2018-11-10 13:04:59 -0600

    factor code

--- ARKit+CoreLocation/ViewController.swift
+++ ARKit+CoreLocation/ViewController.swift
@@ -136,50 +136,15 @@ class ViewController: UIViewController, MKMapViewDelegate, SceneLocationViewDele
     }
     
     @objc func updatePlace() {
-        let pinCoordinate = CLLocationCoordinate2D(latitude: 43.073676, longitude: -89.400900)
-        let pinLocation = CLLocation(coordinate: pinCoordinate, altitude: 266)
-        let annotationNode = LocationAnnotationNode(location: pinLocation, image: textToImage(drawText: "Chadb", size: 75))
+        var list : Array<LocationAnnotationNode> = Array()
+        list.append(MapLabel(name: "Chad", latitude: 43.073676, longitude: -89.400900).getNode())
         
-        self.sceneLocationView.addLocationNodeWithConfirmedLocation(locationNode: annotationNode)
+        for mapLabel in list {
+             self.sceneLocationView.addLocationNodeWithConfirmedLocation(locationNode: mapLabel)
+        }
     }
+
     
-    func textToImage(drawText name: NSString, size: CGFloat) -> UIImage {
-        
-        //text attributes
-        let font=UIFont(name: "Courier-Bold", size: size)!
-        let text_style=NSMutableParagraphStyle()
-        text_style.alignment=NSTextAlignment.center
-        let text_color=UIColor(white: CGFloat(0), alpha: CGFloat(1))
-        let attributes=[NSAttributedString.Key.font:font, NSAttributedString.Key.paragraphStyle:text_style, NSAttributedString.Key.foregroundColor:text_color]
-        
-        let size = CGSize(width: CGFloat(size * CGFloat(name.length)*0.75), height: font.lineHeight*2)
-        //draw image first
-        UIGraphicsBeginImageContext(size)
-        
-        let rect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
-        let ctx: CGContext = UIGraphicsGetCurrentContext()!
-        ctx.setFillColor(gray:1, alpha: 1)
-        
-        
-        ctx.addPath(UIBezierPath(roundedRect: rect, cornerRadius: 10).cgPath)
-        ctx.closePath()
-        ctx.fillPath()
-        ctx.setStrokeColor(red: 0, green: 0, blue: 0, alpha: 1)
-        ctx.addPath(UIBezierPath(roundedRect: rect, cornerRadius: 10).cgPath)
-        ctx.closePath()
-        ctx.strokePath()
-        
-        //vertically center (depending on font)
-        let text_h=font.lineHeight
-        
-        let text_rect_name=CGRect(x: 0, y: (size.height-text_h)/4, width: size.width, height: text_h)
-        
-        name.draw(in: text_rect_name.integral, withAttributes: attributes)
-        
-        let result=UIGraphicsGetImageFromCurrentImageContext()
-        
-        return result!
-    }
     
     @objc func updateUserLocation() {
         if let currentLocation = sceneLocationView.currentLocation() {
@@ -308,4 +273,66 @@ extension UIView {
         
         return recursiveSubviews
     }
+}
+
+
+class MapLabel{
+    let name: NSString
+    let latitude:CLLocationDegrees
+    let longitude:CLLocationDegrees
+    
+    public init(name: NSString, latitude: CLLocationDegrees, longitude: CLLocationDegrees){
+        self.name =  name;
+        self.latitude = latitude;
+        self.longitude = longitude;
+    }
+    
+    
+    
+    func getNode() -> LocationAnnotationNode{
+        let pinCoordinate = CLLocationCoordinate2D(latitude: self.latitude, longitude: self.longitude)
+        let pinLocation = CLLocation(coordinate: pinCoordinate, altitude: 266)
+        let annotationNode = LocationAnnotationNode(location: pinLocation, image: self.getImage(size: 750))
+        annotationNode.scaleRelativeToDistance = true
+        
+        return annotationNode
+    }
+    
+    func getImage(size: CGFloat) -> UIImage {
+        //text attributes
+        let font=UIFont(name: "Courier-Bold", size: size)!
+        let text_style=NSMutableParagraphStyle()
+        text_style.alignment=NSTextAlignment.center
+        let text_color=UIColor(white: CGFloat(0), alpha: CGFloat(1))
+        let attributes=[NSAttributedString.Key.font:font, NSAttributedString.Key.paragraphStyle:text_style, NSAttributedString.Key.foregroundColor:text_color]
+        
+        let size = CGSize(width: CGFloat(size * CGFloat(self.name.length)*0.75), height: font.lineHeight*2)
+        //draw image first
+        UIGraphicsBeginImageContext(size)
+        
+        let rect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
+        let ctx: CGContext = UIGraphicsGetCurrentContext()!
+        ctx.setFillColor(gray:1, alpha: 1)
+        
+        
+        ctx.addPath(UIBezierPath(roundedRect: rect, cornerRadius: 10).cgPath)
+        ctx.closePath()
+        ctx.fillPath()
+        ctx.setStrokeColor(red: 0, green: 0, blue: 0, alpha: 1)
+        ctx.addPath(UIBezierPath(roundedRect: rect, cornerRadius: 10).cgPath)
+        ctx.closePath()
+        ctx.strokePath()
+        
+        //vertically center (depending on font)
+        let text_h=font.lineHeight
+        
+        let text_rect_name=CGRect(x: 0, y: (size.height-text_h)/4, width: size.width, height: text_h)
+        
+        name.draw(in: text_rect_name.integral, withAttributes: attributes)
+        
+        let result=UIGraphicsGetImageFromCurrentImageContext()
+        
+        return result!
+    }
+    
 }
